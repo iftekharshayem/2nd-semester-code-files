bf29114182198048c31c9d0cfbf269a1dda5ebca
Iftekhar shayem <17beyondc@gmail.com>
2026-02-14 10:28:31 +0600

    added test code

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ cout<< "what the heck is going on here";
+    
+
+    return 0;
+}
